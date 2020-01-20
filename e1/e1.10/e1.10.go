5bc6970f579d4fce9e6f4834c41126f47710914c
chen zhengwei <chenzhengwei@inspur.com>
2020-01-20 11:10:26 +0800

    添加e1.10

---
+++ e1/e1.10/e1.10.go
@@ -0,0 +1,57 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 17.
+//!+
+
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	fileName := fmt.Sprintf("test-%s.txt", time.Now().Format("2006-01-02-15-04-05"))
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("open file fail err:", err)
+		return
+	}
+	writer := bufio.NewWriter(file)
+	nbytes, err := io.Copy(writer, resp.Body)
+	writer.Flush()
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
+
+//!-
